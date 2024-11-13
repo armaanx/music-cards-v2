cced146f8515ec122a4f9887a71b85d58c7c02a5
Armaan Mishra <armaanmishra48@gmail.com>
2024-11-13 05:47:42 +0530

    Refactor Footer component to include background color and adjust max width

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div className="w-full border-t h-14 p-2">
-      <div className="w-full max-w-4xl mx-auto flex flex-col items-center justify-center text-xs gap-1">
+    <div className="w-full border-t h-14 p-2 bg-background">
+      <div className="w-full max-w-5xl mx-auto flex flex-col items-center justify-center text-xs gap-1">
         <h1>
           Made by <Link href={"/"}>Armaan</Link>
         </h1>
