650f11c5fa6602ca1b9ec994f1675e43a5c1a99d
Armaan Mishra <armaanmishra48@gmail.com>
2024-11-15 06:02:35 +0530

    Add Main component to display user stats after session validation

---
+++ src/app/main/page.tsx
@@ -0,0 +1,18 @@
+import TopStats from "@/components/TopStats";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+
+export default async function Main() {
+  const session = await getServerSession(authOptions);
+  if (!session || !session.user) {
+    redirect("/");
+  }
+  return (
+    <div className="w-full">
+      <div className="w-full max-w-4xl mx-auto">
+        <TopStats username={session.user.name!} userImg={session.user.image!} />
+      </div>
+    </div>
+  );
+}
