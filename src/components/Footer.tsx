19589e8a888660c61bce2c4ad92371df644c043c
Armaan Mishra <armaanmishra48@gmail.com>
2024-11-17 01:11:09 +0530

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,15 +2,25 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div className="w-full border-t h-14 p-2 bg-background">
-      <div className="w-full max-w-5xl mx-auto flex flex-col items-center justify-center text-xs gap-1">
-        <h1>
-          Made by <Link href={"/"}>Armaan</Link>
-        </h1>
-        <div className="flex flex-row items-center justify-center gap-2">
-          <Link href={"/"}>About</Link>
-          <Link href={"/"}>Privacy Policy</Link>
-          <Link href={"/"}>Contact</Link>
+    <div className="w-full border-t h-14 p-2 bg-background flex flex-col items-center justify-center text-xs">
+      <div className="w-full max-w-5xl mx-auto flex flex-col items-center justify-center  gap-1">
+        <h1>Made with ❤️</h1>
+        <div className="flex flex-row items-center justify-center gap-3">
+          <Link href={"/about"} className="hover:text-blue-400 hover:underline">
+            About
+          </Link>
+          <Link
+            href={"/contact"}
+            className="hover:text-blue-400 hover:underline"
+          >
+            Contact
+          </Link>
+          <Link
+            href={"/privacy"}
+            className="hover:text-blue-400 hover:underline"
+          >
+            Privacy Policy
+          </Link>
         </div>
       </div>
     </div>
